e61f01eb32887400259fc1bbc7c0f486e8450c70
Yghaa <89315059+Yghaa@users.noreply.github.com>
2022-08-13 14:16:55 +0700

    Update and rename guicol.sh to recola.sh

--- guicol.sh
+++
@@ -1,5 +0,0 @@
-!pip install -U colabcode
-
-import colabcode as cc
-cc.ColabCode(port = 12000, 
-             password="unyil", authtoken='2DI0sVjh1Stpo3OKsSJ2sx5IYWG_75Ne6SWc6PQ4bRtaUYnVL')
---
+++ recola.sh
@@ -0,0 +1,3 @@
+pip install git+https://github.com/SwayamJoshi87/remocolab.xrdp.git
+import remocolab
+remocolab.setupVNC()re
